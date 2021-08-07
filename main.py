f56ddc2ce53eec311a6d6ab15bc9689dba5d5c3c
jorgesoft <jorgecarrilloit@gmail.com>
2021-08-07 17:03:47 -0400

    change date placement

--- main.py
+++ main.py
@@ -8,16 +8,18 @@ import time
 
 #disables ssl warnings and set the time for the reports
 requests.packages.urllib3.disable_warnings() 
-now = datetime.now()
-dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
 
 def main():
     while True:
+        #sets the time for the current test
+        now = datetime.now()
+        dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
         for service in config.services:
             check_service(service, config.services.get(service))
 
         if config.errors == 0:
             sendTeams(config.reportHook, dt_string, config.upServices)
+        #waits for next test and resets the services uptime and errors to 0
         time.sleep(config.timer)
         config.upServices = []
         config.errors = 0
